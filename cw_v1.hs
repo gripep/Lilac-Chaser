cd2962d4c0528eab7d1d5e512a88b68bf86f4c82
gripepi <gian.ripepi96@gmail.com>
2019-11-05 13:28:38 +0000

    renamed cw.hs to cw_v1.hs

--- cw_v1.hs
+++ cw_v1.hs
@@ -4,24 +4,24 @@ import Animation
 
 pic :: Animation
 pic = withPaint (always gray)
-    (rect (always 1500) (always 600))
+    (rect (always 800) (always 600))
     `plus`
-    translate (always (625, 250))
+    translate (always (425, 250))
         (rect (always 5) (always 35)) 
     `plus`
-    translate (always (610, 265))
+    translate (always (410, 265))
         (rect (always 35) (always 5))
     `plus`
-    translate (always (625, 275))
+    translate (always (425, 275))
         (combine 
             [translate (always (200*sin((pi / 3.0) * (i / 2.0)), 200*cos((pi / 3.0) * (i / 2.0))))
                 (withPaint (always fuchsia) ((circle (always 20))))
             | i <- [1..12]])
     `plus`
-    translate (always (625, 275))
+    translate (always (425, 275))
         (translate (cycleSmooth 0.5
             [(200*sin((pi / 3.0) * (i / 2.0)), 200*cos((pi / 3.0) * (i / 2.0))) | i <- [1..12]])
         (withPaint (always gray) (circle (always 20))))
 
 save :: IO()
-save = writeFile "lilac.svg" (svg 1500 600 pic)
+save = writeFile "lilac.svg" (svg 800 600 pic)
