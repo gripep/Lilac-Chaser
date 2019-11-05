449b251c3fdf1cc62216b16decaef85f678fa44c
gripepi <gian.ripepi96@gmail.com>
2019-11-05 13:43:06 +0000

    editing cw_v2.hs

--- cw_v2.hs
+++ cw_v2.hs
@@ -2,5 +2,16 @@ module CW where
 
 import Animation
 
+pic :: Animation
+pic = withPaint (always gray)
+    (rect (always 800) (always 600))
+    `plus`
+    translate (always (400, 285))
+        (rect (always 5) (always 40)) 
+    `plus`
+    translate (always (385, 300))
+        (rect (always 35) (always 5))
+    -- `plus`
+
 save :: IO()
 save = writeFile "lilac.svg" (svg 800 600 pic)
