5a578adbd9be51dbf449d001ce5e4184fc6ccc87
gripepi <gian.ripepi96@gmail.com>
2019-11-05 14:32:32 +0000

    Ross edits cw_v2.hs

--- cw_v2.hs
+++ cw_v2.hs
@@ -3,15 +3,19 @@ module CW where
 import Animation
 
 pic :: Animation
-pic = withPaint (always gray)
-    (rect (always 800) (always 600))
+pic = translate (always (400, 300)
+    (withPaint (always gray)
+        (rect (always 800) (always 600))
     `plus`
-    translate (always (400, 285))
-        (rect (always 5) (always 40)) 
+    translate (always (0, -17.5))
+        (rect (always 4) (always 35)) 
     `plus`
-    translate (always (385, 300))
-        (rect (always 35) (always 5))
-    -- `plus`
+    translate (always (-17.5, 0))
+        (rect (always 35) (always 4))
+    `plus`
+    rotate (spinner 36)
+        (translate (always (250, 0))
+            (withPaint (always fuchsia) (circle (always 20))))
 
 save :: IO()
 save = writeFile "lilac.svg" (svg 800 600 pic)
