7878791e7670f8835d43efa9d0248aeb749e9de1
Kavishan <kavishan47@gmail.com>
2019-11-17 16:23:45 +0000

    fixed spelling mistake

--- MyAnimation.hs
+++ MyAnimation.hs
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------------
 -- 
 -- Name     :  Coursework for Functional Programming
--- Authors  :  Kavishan Prabarajah - kavishn.prabarajah@city.ac.uk
+-- Authors  :  Kavishan Prabarajah - kavishan.prabarajah@city.ac.uk
 --             Gianfranco Ripepi - gianfranco.ripepi@city.ac.uk
 --
 -----------------------------------------------------------------------------
